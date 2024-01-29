600df22c4bcf8b507ada61da3a43fafa01107152
ChubenkoYaroslav <135649342+ChubenkoYaroslav@users.noreply.github.com>
2024-01-29 21:14:21 +0200

    Update FingerNumber.js

--- FingerNumber.js
+++ FingerNumber.js
@@ -1,9 +1,16 @@
 const FingerN = parseInt(prompt("Enter your finger number"), 10);
 console.log(FingerN);
+
 if (FingerN === 1) {
   alert("It is a thumb");
 } else if (FingerN === 2) {
   alert("This is the index finger");
+} else if (FingerN === 3) {
+  alert("This is the middle finger");
+} else if (FingerN === 4) {
+  alert("This is the ring finger");
+} else if (FingerN === 5) {
+  alert("This is the little finger");
 } else {
   alert("You don't have any more fingers!");
 }
