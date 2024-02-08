1da61e8e3baa1376b9dfa6ac92ee1c0e8db736a9
ChubenkoYaroslav <135649342+ChubenkoYaroslav@users.noreply.github.com>
2024-02-08 22:05:04 +0200

    Update Filter.js

--- Filter.js
+++ Filter.js
@@ -1,7 +1,8 @@
 const firstArray = [false, 12, '23424', false, 324, '', true, null, 5, -0, 0, 9, undefined, NaN, 6.354, undefined, 7, null];
 
 const myArray = firstArray.filter(i => {
-  return i !== null && i !== 0 && i !== false && i !== undefined && !Number.isNaN(i);
+  
+  return i !== null && i !== 0 && i !== false && i !== undefined && i !== '' && !Number.isNaN(i);
 });
 
 console.log(myArray);
